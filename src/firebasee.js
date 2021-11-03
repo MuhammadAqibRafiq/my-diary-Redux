b35238accd57b9721c0b69102c2348a4a0f6c62d
MuhammadAqibRafiq <77021708+MuhammadAqibRafiq@users.noreply.github.com>
2021-11-03 14:16:31 +0500

    Update firebasee.js

---
+++ src/firebasee.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+    apiKey: "********************",
+    authDomain: "********************",
+    projectId: "********************",
+    storageBucket: "********************",
+    messagingSenderId: "********************",
+    appId: "********************"
+};
+firebase.initializeApp(firebaseConfig)
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+
+export { auth, db };
